5f9436128a0e755f793134f89cc2bd2a496725fe
sausame <bi_song@163.com>
2018-06-28 17:50:45 +0800

    Add record

--- speech.py
+++ speech.py
@@ -1,9 +1,84 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import subprocess
+import sys
+import time
+
 from aip import AipSpeech
 from utils import getProperty, runCommand, OutputPath
+
+try:
+    import alsaaudio
+
+    def record(filename, seconds=5, channels=1, rate=16000):
+
+        print('alsaaudio')
+
+        FORMAT = alsaaudio.PCM_FORMAT_S16_LE
+        TRUCK = 160
+
+        inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK)
+
+        inp.setchannels(channels)
+        inp.setrate(rate)
+        inp.setformat(FORMAT)
+        inp.setperiodsize(TRUCK)
+
+        fp = open(filename, 'wb')
+
+        try:
+            for loops in range(1000000 * seconds):
+
+                # Read data from device
+                l, data = inp.read()
+              
+                if l:
+                    fp.write(data)
+                    time.sleep(.001)
+
+        except KeyboardInterrupt:
+            pass
+        except Exception as e:
+            print(e)
+        finally:
+            fp.close()
+
+except:
+
+    import pyaudio
+
+    def record(filename, seconds=5, channels=1, rate=16000):
+
+        print('pyaudio')
+
+        FORMAT = pyaudio.paInt16
+        CHUNK = 1024
+
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=FORMAT,
+                        channels=channels,
+                        rate=rate,
+                        input=True,
+                        frames_per_buffer=CHUNK)
+
+        fp = open(filename, 'wb')
+
+        try:
+            for i in range(0, int(rate / CHUNK * seconds)):
+                data = stream.read(CHUNK)
+                fp.write(data)
+        except KeyboardInterrupt:
+            pass
+        except Exception as e:
+            print(e)
+        finally:
+            fp.close()
+
+            stream.stop_stream()
+            stream.close()
+
+            p.terminate()
 
 class Speech:
 
